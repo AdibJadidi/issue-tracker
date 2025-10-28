b9e5ded5aa03e88c21e21e7b93824637c3e0d1f7
adoool <adibjadidi74@gmail.com>
2025-10-28 13:15:37 +0330

    Build the edit issue page

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,11 +1,13 @@
 import { Skeleton } from "@/app/components";
-import { Box } from "@radix-ui/themes";
+import { Box, Container } from "@radix-ui/themes";
 const LoadingNewIssuePage = () => {
   return (
-    <Box className={"max-w-xl"}>
-      <Skeleton />
-      <Skeleton height={"20rem"} />
-    </Box>
+    <Container mx={{ initial: "5", xs: "6", sm: "7", md: "9" }}>
+      <Box className={"max-w-xl"}>
+        <Skeleton />
+        <Skeleton height={"20rem"} />
+      </Box>
+    </Container>
   );
 };
 
