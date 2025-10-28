998ea5c67d97720918a9e5f29bc44257a1ebefd0
adoool <adibjadidi74@gmail.com>
2025-10-28 12:08:07 +0330

    REFACTOR: apply SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,6 +5,8 @@ import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetail from "./IssueDetail";
 
 interface Props {
   params: {
@@ -23,20 +25,10 @@ const IssueDetailPage = async ({ params }: Props) => {
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap={"4"}>
       <Box>
-        <Heading>{issue?.title}</Heading>
-        <Flex gap={"2"} my={"2"}>
-          <IssueStatusBadge status={issue?.status} />
-          <p>{issue?.createdAt.toDateString()}</p>
-        </Flex>
-        <Card className="prose" mt={"4"}>
-          <ReactMarkdown>{issue?.description}</ReactMarkdown>
-        </Card>
+        <IssueDetail issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue?.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton issueId={issue?.id} />
       </Box>
     </Grid>
   );
