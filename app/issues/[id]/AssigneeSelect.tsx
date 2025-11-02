b4ff24dcb09ebb6d5f753c96c36d2622da819b37
adoool <adibjadidi74@gmail.com>
2025-11-02 19:03:51 +0330

    Assignee an issue to an user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { Select } from "@radix-ui/themes";
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Skeleton from "react-loading-skeleton";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -20,12 +20,21 @@ const AssigneeSelect = () => {
 
   if (isLoading) return <Skeleton className="h-8 w-full" />;
   if (error) return <div>Error loading users</div>;
+
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(userId) => {
+        axios.patch(`/api/issue/${issue.id}`, {
+          assignedToUserId: userId === "unassigned" ? null : userId,
+        });
+      }}
+      defaultValue={issue.assignedToUserId || "unassigned"}
+    >
       <Select.Trigger placeholder="Assignee" />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
+          <Select.Item value={"unassigned"}>Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -30,7 +30,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box className="md:col-span-1">
           <Flex gap="2" direction={"column"}>
-            <AssigneeSelect />
+            <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue?.id} />
             <DeleteIssueButton issueId={issue?.id} />
           </Flex>
