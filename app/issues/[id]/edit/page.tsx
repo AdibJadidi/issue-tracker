614e85a6ce36cc1a84613e4d6dac327b6c93313d
adoool <adibjadidi74@gmail.com>
2025-11-06 20:04:25 +0330

    Fix getting base URL

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -2,6 +2,15 @@ import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "../../_components/IssueFormSkeleton";
 
+function getBaseUrl() {
+  // Use Vercel's own URL for production
+  if (process.env.VERCEL_URL) {
+    // VERCEL_URL does NOT include the protocol (e.g., just 'issue-tracker-six-brown.vercel.app')
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  // Fallback for local development
+  return process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+}
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
   loading: () => <IssueFormSkeleton />,
@@ -12,11 +21,7 @@ export default async function EditIssuePage({
 }: {
   params: { id: string };
 }) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"}/api/issue/${
-      params.id
-    }`
-  );
+  const response = await fetch(`${getBaseUrl()}/api/issue/${params.id}`);
 
   if (!response.ok) {
     notFound();
